92f782dc40cdc5052d8af3a0d82449b2b1fa8256
haya-alwarthan <111225040+haya-alwarthan@users.noreply.github.com>
2023-01-18 22:24:13 +0300

    minimize the app

--- processing_dataset/pollution.py
+++
@@ -1,9 +0,0 @@
-class PollutionMark:
-  def __init__(self, name, img_path,class_num,bbox,lat,lon):
-    self.name = name
-    self.img_path = img_path
-    self.class_num=class_num
-    self.bbox=bbox
-    self.lat=self.lat
-    self.lon= lon
-
--- test.py
+++
@@ -1,7 +0,0 @@
-import requests
-BASE ='http://127.0.0.1:5000/'
-
-# response = requests.put(BASE +'video/2',data={'name':'fuck like','likes':12,'views':122})
-response = requests.get(BASE +'api/stats/total')
-
-print(response.json())
